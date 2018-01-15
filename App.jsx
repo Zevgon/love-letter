2bbf0f44a053840bb2f15c29904dab192e275daa
Jeremy Leu <jeremyleu@yahoo.com>
2018-01-14 20:50:43 -0800

    eslinter and change app js to jsx

--- App.jsx
+++ App.jsx
@@ -1,6 +1,16 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+
 export default class App extends React.Component {
   render() {
     return (
@@ -12,12 +22,3 @@ export default class App extends React.Component {
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
